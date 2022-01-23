dae2496bceedbd5628b68d728c620d7ef15103dd
Aleksa Bubanja <aleksa3b@gmail.com>
2022-01-23 16:25:04 +0100

    details page finished

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -24,35 +24,68 @@ function CountryDetails() {
   return (
     <>
       <section>
-        {country.map((c) => {
-          console.log(c);
+        {country.map((country) => {
+          console.log(country);
           return (
-            <div key={c.ccn3} className="details">
-              <div className="flag">
-                <Link to="/" id="back">
-                  <svg
-                    width="1em"
-                    height="1em"
-                    viewBox="0 0 16 16"
-                    className="bi bi-arrow-left"
-                    fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5.854 4.646a.5.5 0 0 1 0 .708L3.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0z"
-                    />
-                    <path
-                      fillRule="evenodd"
-                      d="M2.5 8a.5.5 0 0 1 .5-.5h10.5a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-                    />
-                  </svg>
-                  <span>Back</span>
-                </Link>
-                <img src={c.flags.svg} alt="IMG" />
-              </div>
-              <div>
-                <h1>AA</h1>
+            <div key={country.ccn3} className="details">
+              <Link to="/" id="back">
+                <svg
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 16 16"
+                  className="bi bi-arrow-left"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5.854 4.646a.5.5 0 0 1 0 .708L3.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0z"
+                  />
+                  <path
+                    fillRule="evenodd"
+                    d="M2.5 8a.5.5 0 0 1 .5-.5h10.5a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+                  />
+                </svg>
+                <span>Back</span>
+              </Link>
+              <div className="main">
+                <img src={country.flags.svg} alt="IMG" />
+                <div className="info">
+                  <h1>{country.name.common}</h1>
+                  <div>
+                    <ul>
+                      <li>
+                        <span>Native name: </span>
+                        {Object.values(country.name.nativeName)[0]?.common}
+                      </li>
+                      <li>
+                        <span>Population:</span> {country.population}
+                      </li>
+                      <li>
+                        <span>Region:</span> {country.region}
+                      </li>
+                      <li>
+                        <span>Sub Region:</span> {country.subregion}
+                      </li>
+                      <li>
+                        <span>Capital:</span> {country.capital}
+                      </li>
+                    </ul>
+                    <ul>
+                      <li>
+                        <span>Top Level Domain:</span> {country.tld[0]}
+                      </li>
+                      <li>
+                        <span>Currencies: </span>
+                        {Object.values(country.currencies)[0]?.name}
+                      </li>
+                      <li>
+                        <span>Languages: </span>
+                        {Object.values(country.languages).join(", ")}
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
           );
