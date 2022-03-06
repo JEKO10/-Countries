39b273e562c60e5e9b1b36e7fec674d1ea7e7cd5
Aleksa Bubanja <aleksa3b@gmail.com>
2022-03-06 17:53:46 +0100

    adding theme to local storage

--- src/context.js
+++ src/context.js
@@ -1,10 +1,19 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 
 const AppContext = React.createContext();
 
+const getUserTheme = () => {
+  const result = JSON.parse(localStorage.getItem("theme"));
+  return result;
+};
+
 function AppProvider({ children }) {
   const [countries, setCountries] = useState([]);
-  const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useState(getUserTheme());
+
+  useEffect(() => {
+    localStorage.setItem("theme", JSON.stringify(isDark));
+  }, [isDark]);
 
   return (
     <AppContext.Provider value={{ countries, setCountries, isDark, setIsDark }}>
